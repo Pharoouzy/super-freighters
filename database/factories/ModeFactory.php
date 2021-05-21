a51143493b98aef41da0d91f070df9a2044363f6
Pharoouzy <pharoouzy@yahoo.com>
2021-05-21 22:47:06 +0100

    Create models, controllers, migrations, factories and seeders

---
+++ database/factories/ModeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Mode;
+use Faker\Generator as Faker;
+
+$factory->define(Mode::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
